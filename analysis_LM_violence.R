ee84ca3f34fea6f46f75d66fdaa2b89184507ad0
Lina Montoya <lmontoya@berkeley.edu>
2016-04-30 12:40:42 -0700

    latest analysis + bootstrap

--- analysis_LM_violence.R
+++ analysis_LM_violence.R
@@ -5,7 +5,7 @@ library("tmle")
 load("data/analysis-dataset.RData")
 load("data/covariates.RData")
 
-SL.library = c("SL.glm", "SL.step", "SL.glm.interaction")
+SL.library = c("SL.step")
 
 #Dealing with missing data for outcome
 data = data[!is.na(data$any_violence),]
@@ -69,3 +69,47 @@ PsiHat.TMLE = mean(Qbar1W.star) - mean(Qbar0W.star)
 
 #Using function
 PsiHat.TMLE2 = tmle(Y=data$any_violence, A=data$treatment, W=W, Q=cbind(Qbar0W, Qbar1W), g1W=gHat1W, family="binomial")
+PsiHat.TMLE2$estimates$ATE
+
+B = 500
+estimates = data.frame(matrix(NA, nrow = B, ncol = 3))
+for(b in 1:B){
+  
+  bootIndices = sample(1:n, replace = T)
+  bootData = data[bootIndices,]
+  X = subset(bootData, select = -c(studyid, any_arrest, any_violence))
+  X = data.frame(model.matrix(~ . -1 , X))
+  X1 = X0 = X
+  X1$treatment = 1
+  X0$treatment = 0
+  newdata = rbind(X, X1, X0)
+  Qinit = SuperLearner(Y=bootData$any_violence, X=X, newX=newdata, SL.library=SL.library, family="binomial")
+  QbarAW = Qinit$SL.predict[1:n]
+  Qbar1W = Qinit$SL.predict[(n+1): (2*n)] 
+  Qbar0W = Qinit$SL.predict[(2*n+1): (3*n)] 
+  PsiHat.SS.b = mean(Qbar1W - Qbar0W)
+  
+  W = subset(X, select = -c(treatment))
+  gHatSL = SuperLearner(Y=bootData$treatment, X=W, SL.library=SL.library, family="binomial")
+  gHat1W = gHatSL$SL.predict
+  gHat0W = 1- gHat1W
+  
+  PsiHat.IPTW.b = mean(bootData$treatment*bootData$any_violence/gHat1W) - mean((1-bootData$treatment)*bootData$any_violence/gHat0W)
+  
+  H.AW = (2*bootData$treatment-1)/ gHatAW
+  H.1W = 1/gHat1W 
+  H.0W = -1/gHat0W
+  
+  logitUpdate = glm(bootData$any_violence ~ -1 +offset(qlogis(QbarAW)) + H.AW, family=binomial) 
+  eps = logitUpdate$coef
+  
+  QbarAW.star = plogis(qlogis(QbarAW)+ eps*H.AW)
+  Qbar1W.star = plogis(qlogis(Qbar1W)+ eps*H.1W)
+  Qbar0W.star = plogis(qlogis(Qbar0W)+ eps*H.0W) 
+  PsiHat.TMLE.b = mean(Qbar1W.star - Qbar0W.star) 
+  estimates[b,] = c(PsiHat.SS.b, PsiHat.IPTW.b, PsiHat.TMLE.b)
+}
+
+colnames(estimates) = c("SimpSubps", "IPTW", "TMLE")
+
+summary(estimates)
